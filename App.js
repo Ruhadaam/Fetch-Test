1371c8d6a4177d40cc0b645ea73a10692f5d9ede
Ruhadaam <avuclariminicinde@gmail.com>
2024-04-08 17:30:35 +0300

    testing fetch api

--- App.js
+++ App.js
@@ -1,20 +1,43 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View, Image, TouchableOpacity, Alert } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios'; 
 
 export default function App() {
+  const [user, setUser] = useState([]);
+
+  const showAlert = (firstName, lastName) => {
+    Alert.alert(
+      'Kişi',
+      `${firstName} ${lastName}`, // Mesajı birleştirerek göster
+      [
+        { text: 'Tamam', onPress: () => console.log('Tamam pressed') }
+      ],
+      { cancelable: true }
+    );
+  };
+  
+
+  useEffect(() => {
+    axios.get("https://reqres.in/api/users?page=2").then(res => setUser(res.data.data))
+  }, []);
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View className="flex-1 items-center justify-center ">
+      
+      {user.map((value, index) => (
+        <TouchableOpacity onPress={() => showAlert(value.first_name, value.last_name)} key={index}>
+          <View className="border flex flex-row w-64 text-center items-center p-2 m-3" key={index}>
+            <Image
+              className="rounded-full w-8 h-8 mr-2"
+              source={{ uri: value.avatar }}
+            />
+            <Text className="font-medium ">{value.first_name} {value.last_name}</Text>
+          </View>
+        </TouchableOpacity>
+      ))}
+      
       <StatusBar style="auto" />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
